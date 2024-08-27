febfebd8b36a8122532fff98b5a033e34f211f88
AKsamrat <samratuap52@gmail.com>
2024-08-27 06:08:09 +0600

    link add

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const port = process.env.PORT || 9000;
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://bistro-boss-client-pi.vercel.app',
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
